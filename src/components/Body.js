783ef27381c7feafa9cd1f0815185fb1c4c22649
Akhil <agautam@techevolutionpartners.com>
2023-11-07 21:19:39 +0530

    added shimmer ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,9 @@
 import ResturantCard from "./ResturantCard";
-import resList from "../utils/mockData";
 import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
 const Body = () => {
-  const [listOfResturants, setListOfResturants] = useState(resList);
+  const [listOfResturants, setListOfResturants] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -13,7 +14,20 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.4698577&lng=78.3578246&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const jsonData = await data.json();
-    console.log(jsonData);
+    const resturant_list = "restaurant_grid_listing";
+    const restaurantCard = jsonData?.data?.cards?.find(
+      (card) => card.card.card.id === resturant_list
+    );
+
+    const resturantData =
+      restaurantCard?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+
+    console.log(resturantData);
+    setListOfResturants(resturantData);
+    
+    if (listOfResturants.length === 0) {
+      return <Shimmer />;
+    }
   };
   return (
     <div className="body">
