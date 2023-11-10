134db64e25fa227cb240bf54d3aa22bcc55c23db
Akhil <agautam@techevolutionpartners.com>
2023-11-10 14:19:07 +0530

    added routing

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <h3>
+        Welcome to TechInnovate Solutions, where passion, innovation, and
+        excellence converge. Founded in 2015, we are a dedicated team committed
+        to redefining industry standards.
+      </h3>
+    </div>
+  );
+};
+export default About;
